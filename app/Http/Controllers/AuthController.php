9e3a28fa88e1b9a5c7cf540e4a37ea9b81323e39
sontx13 <sontx13@gmail.com>
2023-12-29 09:36:43 +0700

    1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -74,13 +74,6 @@ class AuthController extends Controller
                 ], Response::HTTP_OK);
             }
 
-            return response()->json([
-                'error_code' => 1,
-                'message' => "HTTP_NOT_ACCEPTABLE",
-                'user' => Auth::user(),
-                'id' => Auth::id()
-            ], Response::HTTP_NOT_ACCEPTABLE);
-
             $oClient = OClient::where('password_client', 1)->first();
 
             $token = $this->getTokenAndRefreshTokenByPassword($oClient, \auth()->user()->email, $request->password);
